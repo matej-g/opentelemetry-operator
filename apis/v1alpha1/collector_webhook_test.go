a5ede0673bc7c52d9fb3a48df3232d9d31db1354
Matej Gera <matejgera@gmail.com>
2024-01-04 17:12:24 +0100

    Validate mode and strategy
    Signed-off-by: Matej Gera <matejgera@gmail.com>

--- apis/v1alpha1/collector_webhook_test.go
+++ apis/v1alpha1/collector_webhook_test.go
@@ -413,6 +413,19 @@ func TestOTELColValidatingWebhook(t *testing.T) {
 			expectedErr: "the OpenTelemetry Spec Prometheus configuration is incorrect",
 		},
 		{
+			name: "invalid target allocation strategy",
+			otelcol: OpenTelemetryCollector{
+				Spec: OpenTelemetryCollectorSpec{
+					Mode: ModeDaemonSet,
+					TargetAllocator: OpenTelemetryTargetAllocator{
+						Enabled:            true,
+						AllocationStrategy: OpenTelemetryTargetAllocatorAllocationStrategyLeastWeighted,
+					},
+				},
+			},
+			expectedErr: "does not support the target allocation deployment",
+		},
+		{
 			name: "invalid port name",
 			otelcol: OpenTelemetryCollector{
 				Spec: OpenTelemetryCollectorSpec{
